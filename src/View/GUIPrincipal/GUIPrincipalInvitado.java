2b4336e84e60ccde23506b5e54d847044ba9daa0
AlexisReyes98 <giovanny29.garv@gmail.com>
2020-11-11 17:19:30 -0600

    Relaciones ya con conexion a base de datos

--- src/View/GUIPrincipal/GUIPrincipalInvitado.java
+++ src/View/GUIPrincipal/GUIPrincipalInvitado.java
@@ -9,12 +9,12 @@ package View.GUIPrincipal;
  *
  * @author giova
  */
-public class GUIPrincipal extends javax.swing.JFrame {
+public class GUIPrincipalInvitado extends javax.swing.JFrame {
 
     /**
-     * Creates new form GUIPrincipal
+     * Creates new form GUIPrincipalInvitado
      */
-    public GUIPrincipal() {
+    public GUIPrincipalInvitado() {
         initComponents();
     }
 
@@ -60,20 +60,20 @@ public class GUIPrincipal extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GUIPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIPrincipalInvitado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GUIPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIPrincipalInvitado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GUIPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIPrincipalInvitado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GUIPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUIPrincipalInvitado.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new GUIPrincipal().setVisible(true);
+                new GUIPrincipalInvitado().setVisible(true);
             }
         });
     }
